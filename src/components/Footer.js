481f8bf5aee8c29b48769b01914efe8100d6a914
anand89pandey <57366167+anand89pandey@users.noreply.github.com>
2020-04-28 14:56:50 +0530

    Fixing height of the banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Footer() {
     return(
         <div>
-            <footer class="footer py-4">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-4 text-lg-left"><h2>Created by Anand</h2></div>
-                    <div class="col-lg-4 my-3 my-lg-0">
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+            <footer className="footer py-4">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-4 text-lg-left"><h2>Created by Anand</h2></div>
+                    <div className="col-lg-4 my-3 my-lg-0">
+                        <a className="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a><a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
                     </div>
                     
                 </div>
