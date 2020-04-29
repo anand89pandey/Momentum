d514006e20d4c15b35948b5172131f7907fd8129
anand89pandey <57366167+anand89pandey@users.noreply.github.com>
2020-04-29 20:30:48 +0530

    Created a few more components to make code more readable

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <header className="masthead">
+            <div className="container">
+                <div className="masthead-subheading">Welcome To Promod India</div>
+                <div className="masthead-heading text-uppercase">Thank you for visiting us</div>
+                <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#portfolio">Know Us More</a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
